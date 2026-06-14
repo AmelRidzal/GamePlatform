b542c70acf1e247db1863f3f15f6b668b37c1512
AmelRidzal~ <amelridzal37@gmail.com~>
2026-06-14 20:13:45 +0200

    tictactie service init

---
+++ tictactoeservice/src/main/java/com/gameplatform/tictactoeservice/TictactoeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gameplatform.tictactoeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeserviceApplication.class, args);
+	}
+
+}
